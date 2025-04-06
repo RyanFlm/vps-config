024a356d68a8da79f35f8deceed720721d8cc675
vps-piontekfamily <info@piontekfamily.de>
2025-04-07 01:14:23 +0200

    init flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "NixOS System configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+
+      config = {
+        allowUnfree = true;
+      };
+    };
+
+  in
+  {
+    nixosConfigurations = {
+      vps = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
+
+        modules = [
+          ./nixos/configuration.nix
+        ];
+      };
+    };
+  };
+}
