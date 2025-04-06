d919b59ec0b915a4852f81e745a2a19169fa12ec
vps-piontekfamily <info@piontekfamily.de>
2025-04-07 00:01:08 +0200

    Switch to flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "NixOS System Configuration";
+
+  inputs = {
+    # If you want to use the latest upstream version, I recommend using
+    # branch "nixos-unstable" instead of "23.11" (the latest stable release).
+    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      # This should correspond to the hostname of the machine
+      vps = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+      };
+    };
+  };
+}
